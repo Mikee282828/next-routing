7490ab4da0d269c2829ca757cb4190e03c05a921
MikeDesktop <micheledong2803@gmail.com>
2025-02-23 20:36:08 +0100

    first page

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-    
-   </div>
+      <div className="flex justify-center items-center h-screen text-2xl text-red-500">
+        <div>pagina principale</div>
+      </div>
   );
 }
